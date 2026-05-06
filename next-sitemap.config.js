ef3cf02dc05ac6f5f9bd720978813c61a11b7840
shekelstrong <business.nedopekin@gmail.com>
2026-05-06 23:08:59 +0300

    seo: add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nemo-blog.vercel.app',
+  generateRobotsTxt: false,
+  generateIndexSitemap: false,
+  outDir: './public',
+}
