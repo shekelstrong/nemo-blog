0b207e439a5a969d0ed5de1748f3362cf72ad6de
shekelstrong <shekelstrong@users.noreply.github.com>
2026-05-08 02:25:13 +0300

    SEO: clean sitemap.xml with 24 real URLs only, disable next-sitemap overwrite

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,34 +4,10 @@ module.exports = {
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './public',
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/'],
-      },
-    ],
-    host: 'https://nemo-blog.vercel.app',
-  },
-  transform: async (config, path) => {
-    // Custom priority for different pages
-    let priority = 0.7
-    let changefreq = 'weekly'
-
-    if (path === '/') {
-      priority = 1.0
-      changefreq = 'daily'
-    } else if (path.startsWith('/articles/')) {
-      priority = 0.8
-      changefreq = 'weekly'
-    }
-
-    return {
-      loc: path,
-      changefreq,
-      priority,
-      lastmod: new Date().toISOString(),
-    }
+  // Don't let next-sitemap overwrite our hand-curated sitemap
+  // We manage sitemap.xml manually with only real article URLs
+  transform: async () => {
+    // Return empty array - we manage sitemap.xml ourselves
+    return []
   },
 }
