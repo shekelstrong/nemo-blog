618f1f31bac140cfcc5a28cda4470e09df5dded2
Hermes <hermes@agent>
2026-05-09 02:04:33 +0300

    fix: reverse article order so newest appear first on homepage

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,7 @@ export default function Index() {
         </div>
 
         <div className="space-y-6">
-          {articles.map((article) => (
+          {[...articles].reverse().map((article) => (
             <Link
               key={article.slug}
               href={`/articles/${article.slug}`}
